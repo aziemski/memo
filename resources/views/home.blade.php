9d6bd1880b726b8f2207ecfc034feede605ab842
az <aziemski@github.com>
2024-11-08 23:47:24 +0100

    Handle empty events

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
+    @if($events->isEmpty())
+        <div class="alert alert-info text-center">
+            No events.
+        </div>
+    @else
     <div id="timeline-embed" class="d-flex align-items-center justify-content-center min-vh-100 p-3"></div>
     <script>
         const timelineData = {
@@ -15,5 +20,6 @@
     </script>
 
     @vite(['resources/js/timeline.js'])
+    @endif
 
 @endsection
