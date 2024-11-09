a31cb9a9e91aa1f02ae43e3f2468bce68a5df624
az <aziemski@github.com>
2024-11-09 19:47:07 +0100

    Allow to create events with empty description

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -99,14 +99,11 @@ class EventController extends Controller
 
     public function store(Request $req)
     {
-        // TODO allow to create events without descriptions
-        // TODO handle sql exceptions nicely
-
         $req->validate([
             'name' => 'required|max:255',
             'start_date' => 'required|date',
             'end_date' => 'required|date|after_or_equal:start_date',
-            'description' => 'string',
+            'description' => 'nullable|string',
             'image' => 'nullable|url',
             'categories' => 'nullable|array',
             'categories.*' => 'exists:categories,id'
